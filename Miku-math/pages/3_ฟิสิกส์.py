cad1633766150b3de6d80aad669cdcfc341b59b4
patcharapoljaronesin-oss <patcharapoljaronesin@gmail.com>
2026-09-14 14:01:19 +0700

    Update 3_ฟิสิกส์.py

--- Miku-math/pages/3_ฟิสิกส์.py
+++ Miku-math/pages/3_ฟิสิกส์.py
@@ -38,63 +38,59 @@ if selected == "สูตรการเคลื่อนที่แนวต�
 
     if check == True and inputCounter == 3:
         if var[0] != "":
-            s = int(var[0])
+            s = float(var[0])
         else:
             s = None
         if var[1] != "":
-            t = int(var[1])
+            t = float(var[1])
         else:
             t = None
         if var[2] != "":
-            u = int(var[2])
+            u = float(var[2])
         else:
             u = None
         if var[3] != "":
-            v = int(var[3])
+            v = float(var[3])
         else:
             v = None
         if var[4] != "":
-            a = int(var[4])
+            a = float(var[4])
         else:
             a = None
             
         # v = u + at
         if t and u and a:
             st.markdown("$v = u + at$")
-            st.markdown(f"$v =$ {u + (a * t)}")
+            st.markdown(f"$v =$ {u + (a * t)} $m/s$")
         # s = ((u + v)/2) * t
         elif t and u and v:
             st.markdown("$s = [(u + v) / 2] * t")
-            st.markdown(f"$s =$ {((u + v) / 2) * t}")
+            st.markdown(f"$s =$ {((u + v) / 2) * t} $m$")
         # s = ut - 1/2at²
         elif u and t and a:
             st.markdown("$s = ut - ½at²$")
-            st.markdown(f"$s =$ {(u * t) - ((0.5) * a * (t**2))}")
+            st.markdown(f"$s =$ {(u * t) - ((0.5) * a * (t**2))} $m$")
         # s = vt - 1/2at²
         elif v and t and a:
             st.markdown("$s = vt - ½at²$")
-            st.markdown(f"$s =$ {(v * t) - ((0.5) * a * (t**2))}")
+            st.markdown(f"$s =$ {(v * t) - ((0.5) * a * (t**2))} $m$")
         # v² = u² + as
         elif u and a and s:
             st.markdown("$v² = u² + as$")
-            st.markdown(f"$v² =$ {(u**2) + (a * s)}")
+            st.markdown(f"$v² =$ {(u**2) + (a * s)} $m/s$")
         # a = (2(s-ut))/t²
         elif s and t and u:
             st.markdown("$a = (2(s - ut)) / t²$")
-            st.markdown(f"$a =$ {(2*(s - (u * t))) / (t**2)}")
+            st.markdown(f"$a =$ {(2*(s - (u * t))) / (t**2)} $m/s²$")
         # a = (2(s-vt))/t²
         elif s and t and v:
             st.markdown("$a = s/vt + 2/t²$")
-            st.markdown(f"$a =$ {(2*(s - (v * t))) / (t**2)}")
+            st.markdown(f"$a =$ {(2*(s - (v * t))) / (t**2)} $m/s²$")
         # a = (v² - u²)/2s
         elif v and u and s:
             st.markdown("$a = (v² - u²) / 2s$")
-            st.markdown(f"$a =$ {(v**2 - u**2) / (2*s)}")
+            st.markdown(f"$a =$ {(v**2 - u**2) / (2*s)} $m/s²$")
         # a = (v - u) / t
         elif v and u and t:
             st.markdown("$a = (v - u) / t$")
-            st.markdown(f"$a =$ {(v - u) / t}")
-
-
-
-            
+            st.markdown(f"$a =$ {(v - u) / t} $m/s²$")
