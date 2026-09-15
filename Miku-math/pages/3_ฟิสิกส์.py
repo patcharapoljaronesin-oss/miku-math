2ddc405a4c1265929de12fcf1054f14f137b3048
patcharapoljaronesin-oss <patcharapoljaronesin@gmail.com>
2026-09-15 17:28:48 +0700

    Update 3_ฟิสิกส์.py

--- Miku-math/pages/3_ฟิสิกส์.py
+++ Miku-math/pages/3_ฟิสิกส์.py
@@ -108,4 +108,6 @@ if selected == "สูตรการเคลื่อนที่แนวต�
         elif u != None and v != None and a != None:
             st.markdown("$t = (v-u)/a$")
             st.markdown(f"$t =$ {(v-u)/a} $s$")
-st.image("https://preview.redd.it/chibi-miku-v0-4dv4gsxdbikf1.jpeg?width=1080&crop=smart&auto=webp&s=31848bea76b2db81d0d9a4228d5e14af558ee061", width=500)
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    st.image("https://preview.redd.it/chibi-miku-v0-4dv4gsxdbikf1.jpeg?width=1080&crop=smart&auto=webp&s=31848bea76b2db81d0d9a4228d5e14af558ee061", width=500)
